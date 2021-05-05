ded0df0827693c13cd4ac03e51678e9a50fc9d14
Jefferson Santos <jefferson.b.santos@gmail.com>
2021-05-05 09:05:50 -0300

    Fixing an import error.

--- 5-sql/py01-connect.py
+++ 5-sql/py01-connect.py
@@ -1,8 +1,11 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+# Antes de executar o código, configure a variável DATABASE_URL 
+# no seu terminal com o comando: 
+# export DATABASE_URL=postgresql://postgres:sua-senha@localhost/nome-do-banco
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
